bf95bad6abecacd51c2b65ee86657fcf503ad8d2
gnaneswar-k <gnaneswar.kulindala@research.iiit.ac.in>
2023-12-17 13:59:50 +0530

    Revert "Edited Metadata."
    This reverts commit f44acccfbb3f6090cfbb2181ec343d3e14661fab.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import StoreProvider from './StoreProvider'
-import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Driving Test - Selection Sort',
-  description: 'An Algodynamics Driving Test for selection sort',
-}
 
 function RootLayout({
   children,
