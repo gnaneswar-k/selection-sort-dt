d601ecab41c1dad92b389ad596fd17fcc5e73250
gnaneswar-k <gnaneswar.kulindala@research.iiit.ac.in>
2023-12-21 10:26:18 +0530

    Updated error.tsx

--- app/experiment/error.tsx
+++ app/experiment/error.tsx
@@ -1,7 +1,7 @@
 'use client' // Error components must be Client Components.
 
 import { useEffect } from 'react'
-import '../globals.css'
+import '../../globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,20 +18,32 @@ export default function Error({
   }, [error])
 
   return (
-    <div className={inter.className + " h-screen overflow-hidden flex flex-col bg-slate-50"}>
-      <span className={"px-4 font-sans text-l font-bold text-gray-950 col-span-4 justify-self-center"}>
-        Something went wrong while loading the experiment.
-      </span>
-      <button
-        type="button"
-        className={"flex m-2 p-3 justify-center items-center rounded-md shadow-md bg-amber-300"}
-        onClick={
-          // Attempt to recover by trying to re-render the segment.
-          () => reset()
-        }
-      >
-        Try Again
-      </button>
-    </div>
+    <html>
+      <body>
+        <header
+          id='headerBlock'
+          className={'grid p-4 grid-cols-4 justify-around bg-gradient-to-r from-sky-600 via-blue-600 to-sky-600  shadow-lg'}
+        >
+          <span className={"px-4 font-sans text-2xl font-bold text-slate-50 col-span-4 justify-self-center"}>
+            Driving Test - Selection Sort
+          </span>
+        </header>
+        <div className={inter.className + " h-screen overflow-hidden flex flex-col justify-center items-center bg-slate-50 space-y-8"}>
+          <span className={"px-4 font-sans text-3xl font-bold text-gray-950 col-span-4 justify-self-center"}>
+            Something went wrong while loading the experiment.
+          </span>
+          <button
+            type="button"
+            className={"flex m-2 p-3 justify-center items-center rounded-md shadow-md bg-amber-300"}
+            onClick={
+              // Attempt to recover by trying to re-render the segment.
+              () => reset()
+            }
+          >
+            Try Again
+          </button>
+        </div>
+      </body>
+    </html>
   )
 }
